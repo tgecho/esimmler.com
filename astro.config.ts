a8c5aee5772d83bf364158de52d32a1c44b954e0
Erik Simmler <git@getpi.org>
2022-12-03 14:55:47 -0500

    Add heading links

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import autolinkHeadings from "rehype-autolink-headings";
+import slug from "rehype-slug"
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,12 @@ export default defineConfig({
   integrations: [mdx(), sitemap()],
   markdown: {
     extendDefaultPlugins: true,
-    rehypePlugins: [[autolinkHeadings, { behavior: "wrap" }]],
+    rehypePlugins: [
+      // Normally astro would inject the ids, but apparently it doesn't happen
+      // until AFTER the autolink plugin runs, so let's do that ourselves:
+      // https://github.com/withastro/astro/issues/5001
+      slug,
+      [autolinkHeadings, { behavior: "wrap" }]
+    ],
   },
 });
