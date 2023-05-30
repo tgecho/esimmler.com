35b2c8f127f45932c6d9b857e67bb1155133ae27
Erik Simmler <git@getpi.org>
2023-05-29 22:20:12 -0400

    Inline small stylesheets

--- astro.config.ts
+++ astro.config.ts
@@ -6,25 +6,32 @@ import markdownIntegration from "@astropub/md";
 
 export default defineConfig({
   site: "https://esimmler.com/",
-  integrations: [sitemap({
-    serialize(item) {
-      item.url = item.url.replace(/\/$/, '')
-      return item;
-    },
-  }), markdownIntegration()],
+  integrations: [
+    sitemap({
+      serialize(item) {
+        item.url = item.url.replace(/\/$/, "");
+        return item;
+      },
+    }),
+    markdownIntegration(),
+  ],
   markdown: {
     rehypePlugins: [
       // Normally astro would inject the ids, but apparently it doesn't happen
       // until AFTER the autolink plugin runs, so let's do that ourselves:
       // https://github.com/withastro/astro/issues/5001
       slug,
-      [autolinkHeadings, { behavior: "wrap" }]
+      [autolinkHeadings, { behavior: "wrap" }],
     ],
   },
   vite: {
     ssr: {
       // some sub-dependencies are still using `require()`
-      external: ["@astropub/md"]
-    }
-  }
+      external: ["@astropub/md"],
+    },
+  },
+  experimental: {
+    inlineStylesheets: `auto`,
+  },
+  compressHTML: true,
 });
