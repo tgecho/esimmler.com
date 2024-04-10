ee89f51b5a40d9e593af882a13c54bfc9b407c64
Erik Simmler <erik@getpi.org>
2024-04-09 21:40:04 -0400

    Fix summaries in rss feeds

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,14 +1,35 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import { getCollection, type CollectionEntry } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { byDate } from "../util/byDate";
 import { getLink } from "../util/getSlug";
 import { getSummary } from "../util/getSummary";
 
 export async function GET() {
+  const postsPrefixLength = "../content/blog/".length;
+  const postImportResult = import.meta.glob("../content/blog/**/*.md", {
+    eager: true,
+  });
+  const rawPostsById = Object.fromEntries(
+    Object.entries(postImportResult).map(([key, value]) => [
+      key.slice(postsPrefixLength),
+      value,
+    ]),
+  ) as Record<string, { compiledContent: () => string }>;
+
+  function getCompiledContent(post: CollectionEntry<"blog">) {
+    const rawPost = rawPostsById[post.id];
+    if (rawPost) {
+      return rawPost.compiledContent();
+    } else {
+      return getSummary(post);
+    }
+  }
+
+  // console.log("postImportResult all", postImportResult);
   const posts = (await getCollection("blog")).sort(byDate);
   const items = (await posts).map(async (post, index) => {
-    const content = index < 10 ? post.compiledContent() : getSummary(post);
+    const content = index < 10 ? getCompiledContent(post) : getSummary(post);
     return {
       title: post.data.title,
       pubDate: post.data.date,
@@ -30,7 +51,4 @@ export async function GET() {
   });
 
   return feed;
-}
-function sanitizeHtml(arg0: any) {
-  throw new Error("Function not implemented.");
 }
