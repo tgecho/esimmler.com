c62221fddf16e4f9b14319a93e8694338fae46f0
Erik Simmler <erik@getpi.org>
2024-12-24 10:51:28 -0500

    Add support for pulling drafts with the admin api

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,3 +5,10 @@ interface ImportMetaEnv {
   readonly GHOST_SERVER_URL: string;
   readonly GHOST_CONTENT_API_KEY: string;
 }
+
+// There aren't any types available, but it's close enough, and we're only using
+// this to get at draft posts, so this should be fine for now.
+declare module "@tryghost/admin-api" {
+  import GhostContentAPI from "@tryghost/content-api";
+  export default class GhostAdminAPI extends GhostContentAPI {}
+}
--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,17 +1,34 @@
-import GhostContentAPI from "@tryghost/content-api";
+import GhostContentAPI, { type GhostAPI } from "@tryghost/content-api";
+import GhostAdminAPI from "@tryghost/admin-api";
 
-export const ghostClient = new GhostContentAPI({
-  url: import.meta.env.GHOST_SERVER_URL,
-  key: import.meta.env.GHOST_CONTENT_API_KEY,
-  version: "v5.0",
-});
+const { GHOST_SERVER_URL, GHOST_ADMIN_API_KEY, GHOST_CONTENT_API_KEY } =
+  import.meta.env;
+
+let client: GhostAPI;
+
+if (GHOST_ADMIN_API_KEY) {
+  client = new GhostAdminAPI({
+    url: GHOST_SERVER_URL,
+    key: GHOST_ADMIN_API_KEY,
+    version: "v5.0",
+  });
+} else if (GHOST_CONTENT_API_KEY) {
+  client = new GhostContentAPI({
+    url: GHOST_SERVER_URL,
+    key: GHOST_CONTENT_API_KEY,
+    version: "v5.0",
+  });
+} else {
+  throw new Error("No Ghost API key provided");
+}
 
 export function allPosts(limit: "all" | number = "all") {
-  return ghostClient.posts.browse({
+  console.log("allPosts", limit);
+  return client.posts.browse({
     limit,
   });
 }
 
 export function postFromSlug(slug: string) {
-  return ghostClient.posts.read({ slug }, { include: "tags" });
+  return client.posts.read({ slug }, { include: "tags" });
 }
