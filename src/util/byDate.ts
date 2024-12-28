1d6660382de3b4874781b38114aa4a31ea810d26
Erik Simmler <erik@getpi.org>
2024-12-28 18:13:05 -0500

    Clean out obsolete astro content code

--- src/util/byDate.ts
+++
@@ -1,6 +0,0 @@
-import type { CollectionEntry } from "astro:content";
-
-export function byDate(a: CollectionEntry<"blog">, b: CollectionEntry<"blog">) {
-    return (b.data.date.valueOf() || Infinity) -
-        (a.data.date.valueOf() || Infinity)
-}
